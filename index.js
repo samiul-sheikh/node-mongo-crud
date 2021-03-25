daebcf4ed96d3b7d83a2a4cf7987357b219394f2
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-03-25 17:56:26 +0600

    connect to cloud database successfully

--- index.js
+++ index.js
@@ -2,20 +2,21 @@ const express = require('express');
 
 const app = express();
 
-const password = '';
-
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://organicAdmin:#@cluster0.2uohe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const password = 'test';
+
+const uri = "mongodb+srv://organicAdmin:test@cluster0.2uohe.mongodb.net/organicdb?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get('/', (req, res) => {
-    res.send('hello, I am working');
+  res.send('hello, I am working');
 })
 
 
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
+  const collection = client.db("organicdb").collection("products");
+  console.log('database connected');
   client.close();
 });
 
