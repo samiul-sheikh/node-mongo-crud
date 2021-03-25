cc452e36d18bb9cecd222e8dcef12d505ec06d42
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-03-25 19:56:23 +0600

    send data to database

--- index.js
+++ index.js
@@ -4,20 +4,27 @@ const app = express();
 
 const MongoClient = require('mongodb').MongoClient;
 
-const password = 'test';
+const password = 'test1234';
 
-const uri = "mongodb+srv://organicAdmin:test@cluster0.2uohe.mongodb.net/organicdb?retryWrites=true&w=majority";
+const uri = "mongodb+srv://organicAdmin:test1234@cluster0.2uohe.mongodb.net/organicdb?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+// send file to UI
 app.get('/', (req, res) => {
-  res.send('hello, I am working');
+    res.sendFile(__dirname + '/index.html');
 })
 
 
 client.connect(err => {
-  const collection = client.db("organicdb").collection("products");
-  console.log('database connected');
-  client.close();
+    const collection = client.db("organicdb").collection("products");
+    // send data to database
+    const product = { name: "honey", price: 50, quantity: 10 };
+    collection.insertOne(product)
+        .then(result => {
+            console.log('one product inserted successfully');
+        })
+    console.log('database connected');
+    // client.close();
 });
 
 app.listen(3000);
